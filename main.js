1e4a27e74bdbd8b440366f46ae8c4390184c5045
Diego Garcia <diegogarcianavas.4b@gmail.com>
2021-08-16 14:30:28 +0200

    navbar created

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-import './style.css'
+import "./styles/style.scss"
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+const windowSize = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+const $ = document.querySelector("#app")
+
+const navBar = `
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-menu is-active">
+      <button class="button">Inicio</button>
+      <button class="button">Proyectos</button>
+      <button class="button">Contacto</button>
+    </div>
+  </nav>
 `
+const showSize = () => {
+  console.log(windowSize.width)
+}
+
+const init = () => {
+  const html = `${navBar}`
+
+  $.innerHTML = html
+  window.addEventListener("resize", showSize)
+}
+
+init()
